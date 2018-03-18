be045962bca7202d5f51ebbbdd623a08a816d89f
Brandon VanCamp <brandon@overtimewebdev.com>
2018-03-18 11:11:49 -0500

    Initial Comit

---
+++ app.js
@@ -0,0 +1,10 @@
+const data = [{
+  name: '',
+  email: ''
+}, ];
+
+// const rebuild = () => {
+const name = document.querySelector('H3');
+let nextName = name.parentNode.parentNode.nextElementSibling.firstElementChild.firstElementChild.nextElementSibling;
+
+// }
