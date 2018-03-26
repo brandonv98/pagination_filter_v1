a463c89d5d37d3b1ee1d6f1cb7655baa8be99069
Brandon VanCamp <brandon@overtimewebdev.com>
2018-03-26 07:58:59 -0500

    Add 404 error handleing for no results

--- script.js
+++ script.js
@@ -7,27 +7,35 @@ const handleSearchFormBuild = `
                               <div class="student-search">
                                 <input id="search-value" placeholder="Search for students...">
                                 <button id="search">Search</button>
-                              </div>
-                            `; /// END handleSearchFormBuild.
+                              </div>`; /// END handleSearchFormBuild.
 $headerNode.html(`${handleSearchFormBuild}`); // Append search form.
+// 404 Error Handler.
+const handle404Error = `
+                      <li class="error-404">
+                        <div class="error-details">
+                          <h1>Whoops...</h1>
+                          <span class="no-resaults">Your search result did not find any records.</span>
+                          <span class="error">404 Error, page not found. <span>: (</span></span>
+                        </div>
+                      </li>`; /// END 404 Errors.
 // ----------------------------------------------- //
 //  Now we can slect our form inputs and buttons  //
-const $searchButton = $('button[id="search"]').on('click', (e) => {
+const $searchButton = $('button[id="search"]').on('click', (e) => { // Event listener on search button.
 	const $inputValue = $('input[id="search-value"]').val();
 	const $studentDetail = $students;
 	let $student = $students;
 
 	let HTML = `<ul class="student-list">`; // Create UL
 
-	for (let i = 0; i < $studentDetail.length; i++) { // Loop thew students search.
+	for(let i = 0; i < $studentDetail.length; i++) { // Loop thew students search.
 
-		let studentDetails = $studentDetail[i].firstElementChild; // First div in students list li > div.
+		let studentDetails = $studentDetail[i].firstElementChild; // First div in students list (li > div).
 		let name = studentDetails.firstElementChild.nextElementSibling; // search every name.
 		let imgSrc = name.previousElementSibling; // Student image src link
 		let email = name.nextElementSibling; // Student email address.
 		let joinDate = studentDetails.nextElementSibling; // Student join Date.
 
-		if (name.textContent.indexOf($inputValue) >= 0) { // If user input === match in studnets data. Return all matches. !IMPOTANT only searching threw names.
+		if(name.textContent.indexOf($inputValue) >= 0) { // If user input === match in studnets data. Return all matches. !IMPOTANT only searching threw names.
 			HTML += `<li class="student-item cf">` +
 				`<div class="student-details">` +
 				`<img class="avatar" src="${imgSrc.src}">` +
@@ -39,16 +47,21 @@ const $searchButton = $('button[id="search"]').on('click', (e) => {
 				`</div>` +
 				`</li>`;
 		}
-	} // END OF FOR LOOP!!!!!! ///
-	HTML += `</ul>`; // Finished and button up touch.
+	} // END OF FOR LOOP!  ///
+	HTML += `</ul>`; // Finish and button up touch.
+	let isResults = HTML.length; // Check if data does exist if not throw 404 error.
+	if(isResults < 45) { /// 404 Error
+		HTML = `${handle404Error}`; // Append 404
+	}
+
 
 	$studentUlNode.html(HTML); // Add matched studens in search.
-	console.log(this.$students, $students);
 	$student = $(".student-list li"); // Overright old students with searched students.
 	$(".pagination").remove(); // Remove old navigation
 	onLoad($student); // Create new navigation. Via our callback func.
 });
-////////// End of search.
+
+//////////////////////    End of SEARCH.    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 const populateStudents = (pageNumber, student) => {
 	student.hide(); // Hide all students on the page
 	student.slice((pageNumber * 10) - 10, pageNumber * 10).show(); //
@@ -59,7 +72,7 @@ const handlePageLinks = (student) => {
 
 	let HTML = '<div class="pagination">' +
 		'<ul>';
-	for (let i = 1; i <= pageCount; i++) { /// Populate links
+	for(let i = 1; i <= pageCount; i++) { /// Populate links
 		HTML += '<li><a href="#">' + i + '</a></li>';
 	}
 	HTML += '</ul>' +
